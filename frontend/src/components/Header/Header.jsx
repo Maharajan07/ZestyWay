083123e02e668aa9a13ea63ec875649ea2b29662
Maharajan07 <maharajan03007@gmail.com>
2025-08-04 01:09:19 +0530

    Initial commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Fuel Your Cravings, One Bite at a Time!</h2>
+        <p>
+          Craving delicious food from your favorite local restaurants? We’ve got you covered!
+ZestyWay lets you explore a wide variety of cuisines, place quick orders, and enjoy seamless doorstep delivery – all in just a few clicks.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
